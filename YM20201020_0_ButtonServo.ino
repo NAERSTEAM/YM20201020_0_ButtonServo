9fa050cb0e5961cbc1019cc0f8204dc8f15cd1f6
TIK <firmament_solo@hotmail.com>
2020-10-20 15:30:52 +0800

    Added smaple for multi servos control

--- YM20201020_0_ButtonServo.ino
+++ YM20201020_0_ButtonServo.ino
@@ -1,13 +1,18 @@
+//OCT.20 2020 Added smaple for multi servos control
 //OCT.20 2020 Created this file based on YM20201013_1_LED_MultiKeys
 
 #include<Servo.h>
 
 Servo servo_0;
+Servo servo_1;
+Servo servo_2;
 int key_0=3;
 int key_1=4;
 int key_2=5;
 
 int pin_s0=9;
+int pin_s1=10;
+int pin_s2=11;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,6 +21,8 @@ void setup() {
   pinMode(key_2,INPUT_PULLUP);
 
   servo_0.attach(pin_s0);
+  servo_1.attach(pin_s1);
+  servo_2.attach(pin_s2);
 }
 
 void loop() {
@@ -24,17 +31,21 @@ void loop() {
   if(LOW==digitalRead(key_0))
   {
       servo_0.write(180);
+      servo_1.write(180);
+      servo_2.write(180);
 
   }
   else
   {
-      servo_0.write(0);
+      
 
   }
 
   if(LOW==digitalRead(key_1))
   {
-
+      servo_0.write(180);
+      servo_1.write(0);
+      servo_2.write(180);
   }
   else
   {
@@ -43,13 +54,13 @@ void loop() {
 
   if(LOW==digitalRead(key_2))
   {
-
+      servo_0.write(0);
+      servo_1.write(0);
+      servo_2.write(0);
   }
   else
   {
 
   }
 
-
-  
 }
